6cd8c23a674c73793605e9fedbfc1382acf16702
Komgrit Sungkhaphong <komgritclick@gmail.com>
2025-05-19 19:56:00 -0700

    add Pylon 8 folder for development however, will require \external\pylon 8\Runtime for libraries linker

---
+++ CMakeProject2/external/pylon 8/Development/include/pylon/PylonVersionNumber.h
@@ -0,0 +1,15 @@
+// This file has been generated automatically using:
+// "${buildin.inputfile}"
+// DO NOT EDIT!
+
+#pragma once
+
+#define PYLON_VERSION_MAJOR           9
+#define PYLON_VERSION_MINOR           1
+#define PYLON_VERSION_SUBMINOR        1
+#define PYLON_VERSION_BUILD           279
+#define PYLON_VERSIONSTRING_MAJOR     "9"
+#define PYLON_VERSIONSTRING_MINOR     "1"
+#define PYLON_VERSIONSTRING_SUBMINOR  "1"
+#define PYLON_VERSIONSTRING_BUILD     "279"
+#define PYLON_VERSIONSTRING_EXTENSION "Internal"
