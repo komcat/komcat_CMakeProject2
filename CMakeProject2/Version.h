0d9160a8a6f1732adce26ff17e4fa64fc80b9d86
Komgrit Sungkhaphong <komgritclick@gmail.com>
2025-06-22 17:01:40 -0700

    freeze version 2.1.1.17

--- CMakeProject2/Version.h
+++ CMakeProject2/Version.h
@@ -14,7 +14,7 @@ version 2.1.0.46
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 1
 #define VERSION_PATCH 1
-#define VERSION_BUILD 16
+#define VERSION_BUILD 17
 
 // Build information
 #define BUILD_DATE __DATE__
