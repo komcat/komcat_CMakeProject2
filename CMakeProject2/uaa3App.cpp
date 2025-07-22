77882e699cce082d55608a63dbea376719045b8c
Komgrit Sungkhaphong <komgritclick@gmail.com>
2025-07-22 11:23:35 -0700

    add 2nd camera.

--- CMakeProject2/uaa3App.cpp
+++ CMakeProject2/uaa3App.cpp
@@ -321,8 +321,11 @@ int main(int argc, char* argv[])
 	//cameraManager->InitializeAllCameras();
 
 	// In your main application, try this:
-	auto camera1 = CameraInfo::CreateByIP("main_camera", "192.168.0.68", "Your existing camera");
+	auto camera1 = CameraInfo::CreateByIP("main_camera", "192.168.0.68", "Top view camera");
 	cameraManager->AddCamera(camera1);
+	auto camera1 = CameraInfo::CreateByIP("aux_camera", "192.168.0.69", "Auxilary Camera");
+	cameraManager->AddCamera(camera1);
+
 	cameraManager->InitializeAllCameras();
 
 
