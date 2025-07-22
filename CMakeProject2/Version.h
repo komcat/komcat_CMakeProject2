5cfb1c4ec930b882d1125beb6e08544a3494c6aa
Komgrit Sungkhaphong <komgritclick@gmail.com>
2025-07-22 16:10:01 -0700

    fix base line uaa3app.

--- CMakeProject2/Version.h
+++ CMakeProject2/Version.h
@@ -14,7 +14,7 @@ version 2.1.0.46
 #define VERSION_MAJOR 2
 #define VERSION_MINOR 1
 #define VERSION_PATCH 1
-#define VERSION_BUILD 150
+#define VERSION_BUILD 151
 
 // Build information
 #define BUILD_DATE __DATE__
